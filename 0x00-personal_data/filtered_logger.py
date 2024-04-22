a75ec96a591aed57eca622dcbc9eb6d52613f6a5
kahenyamercy <kahenyamercy5@gmail.com>
2024-04-22 10:31:22 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ Returns a log message obfuscated """
     for f in fields:
-        message = re.sub(f'{f}=.*?{separator}',
-                         f'{f}={redaction}{separator}', message)
+        message: str = re.sub(f'{f}=.*?{separator}',
+                              f'{f}={redaction}{separator}', message)
     return message
-
